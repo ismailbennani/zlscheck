9d79b783199533369615539e1300dfd920c4a756
ismailbennani <ismail.lahkim.bennani@ens.fr>
2020-09-25 18:15:54 +0200

    small change

--- src/scenario/scenario.ml
+++ src/scenario/scenario.ml
@@ -1,4 +1,13 @@
-module type S = Scenario_types.S
+module type S =
+sig
+  type params
+  type output
+
+  val name : string
+  val dim : int
+  val length : params -> float (* time after which output is constant *)
+  val get_interp : params -> (float -> output array)
+end
 
 module Constant(Config : sig type t val dim : int end) =
 struct
